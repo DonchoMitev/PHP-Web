62e998860558b4a722b603c3e78c0761b4a947dc
Doncho Mitev <doncho.mitev@mail.bg>
2018-11-20 14:53:00 +0200

    create login form and navigate to action login

--- PHP_APPS/Blog/src/SoftUniBlogBundle/Controller/SecurityController.php
+++ PHP_APPS/Blog/src/SoftUniBlogBundle/Controller/SecurityController.php
@@ -2,11 +2,16 @@
 
 namespace SoftUniBlogBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SecurityController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * @Route("/login", name="security_login")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function loginAction()
     {
         return $this->render('security/login.html.twig');
     }
